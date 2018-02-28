02a2a338c92b16483a9b25c6106e801f54a98e56
William <dpwilliamj@gmail.com>
2018-02-28 12:53:34 +1100

    Inital commit

---
+++ TotoEV-Demo/Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"6L69O-12D68-3N3L | NRMA | 2", "6L69O-12D68-3N3L | ActewAGL | 3"})
+        Me.ComboBox1.Location = New System.Drawing.Point(115, 76)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(406, 28)
+        Me.ComboBox1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(234, 235)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(149, 81)
+        Me.Button1.TabIndex = 1
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(650, 447)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Button1 As Button
+End Class
---
+++ TotoEV-Demo/Form1.vb
@@ -0,0 +1,120 @@
+﻿Imports System.Net
+Imports System.Text
+Imports Newtonsoft.Json
+Imports Newtonsoft.Json.Linq
+
+Public Class Form1
+    Public AccessToken As String = "174303fb4870ed9400655d3f086ee990"
+    Public ID As String
+    Public InUse As String
+    Public State As String = Nothing
+    Public Sub program_load() Handles MyBase.Load
+        ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls Or SecurityProtocolType.Tls11 Or SecurityProtocolType.Tls12
+    End Sub
+
+    Private Function API_Send(ByRef State As Integer)
+        Dim BodyText As String = Nothing
+
+        BodyText = "{""in_use"":" & State & ",""assigned_to"": 1}"
+        'MsgBox(BodyText)
+        'convert text into ascii
+        Dim encoding As New ASCIIEncoding()
+        Dim Bytes As Byte() = encoding.GetBytes(BodyText)
+        Dim request = TryCast(System.Net.WebRequest.Create("https://server.totoev.com.au/api/cards/" & ID), System.Net.HttpWebRequest)
+
+        request.Method = "PUT"
+        request.Headers.Add("X-Access-Token", AccessToken)
+        request.ContentType = "application/json"
+        request.ContentLength = Bytes.Length
+
+        'write the info into the json body
+        Dim Stream = request.GetRequestStream()
+        Stream.Write(Bytes, 0, Bytes.Length)
+        Stream.Close()
+
+
+        Dim responseContent As String
+        Using response = TryCast(request.GetResponse(), System.Net.HttpWebResponse)
+            Using reader = New System.IO.StreamReader(response.GetResponseStream())
+                responseContent = reader.ReadToEnd()
+                'MessageBox.Show(responseContent)
+            End Using
+        End Using
+
+        Dim json = JsonConvert.DeserializeObject(Of Rootobject)(responseContent)
+
+        If json.data.in_use = 1 Then
+            Button1.Text = "Tap Off"
+        ElseIf json.data.in_use = 0 Then
+            Button1.Text = "Tap On"
+        End If
+        Return True
+    End Function
+
+    Private Function API_Recieve(ByRef CardIDNumber As String)
+        Dim request = TryCast(System.Net.WebRequest.Create("https://server.totoev.com.au/api/cards/" & CardIDNumber), System.Net.HttpWebRequest)
+
+        request.Method = "GET"
+        request.Headers.Add("X-Access-Token", AccessToken)
+
+        Dim responseContent As String
+        Using response = TryCast(request.GetResponse(), System.Net.HttpWebResponse)
+            Using reader = New System.IO.StreamReader(response.GetResponseStream())
+                responseContent = reader.ReadToEnd()
+                'MsgBox(responseContent)
+                'My.Computer.Clipboard.SetText(responseContent)
+            End Using
+        End Using
+
+        Dim json = JsonConvert.DeserializeObject(Of Rootobject)(responseContent)
+        ID = json.data.id
+        InUse = json.data.in_use
+
+        Return True
+    End Function
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        Button1.Text = "Getting card status..."
+        Dim ID As String = ComboBox1.SelectedItem.ToString()
+        Dim CardIDNumber As String = ID.Substring(ID.Length - 1)
+        API_Recieve(CardIDNumber)
+
+        If InUse = 0 Then
+            Button1.Text = "Tap On"
+        Else
+            Button1.Text = "Tap Off"
+        End If
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Button1.Text = "Tap On" Then
+            State = 1
+            API_Send(State)
+            State = Nothing
+        ElseIf Button1.Text = "Tap Off" Then
+            State = 0
+            API_Send(State)
+            State = Nothing
+        End If
+    End Sub
+End Class
+
+
+
+Public Class Rootobject
+    Public Property status As Integer
+    Public Property data As Data
+End Class
+
+Public Class Data
+    Public Property id As Integer
+    Public Property card_number As String
+    Public Property network As String
+    Public Property in_use As String
+    Public Property assigned_to As String
+    Public Property created_at As String
+    Public Property updated_at As String
+End Class
+
+
